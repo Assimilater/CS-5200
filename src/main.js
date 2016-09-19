84ae76245b0fb6e84a8085016f4ba405e62e7f4a
John Call <johnkc1992@gmail.com>
2016-09-19 17:58:30 -0600

    Update main to use newly developed helper modules

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-console.log("hello");
+var path = require('path');
+var conf = require('./conf.js')({
+	defaults: {
+		server: '127.0.0.1',
+		port: 12001,
+	}
+});
+
+var cli = require('./cli.js');
+
+cli.script('server', 'Gets or sets the address of the server to connect to', {
+	usage: 'server [ip|domain]',
+}, function(args) {
+	if (args) {
+		conf.set('server', args);
+		conf.push();
+	}
+	console.log('server address: ' + conf.get('server'));
+})
+
+cli.script('port', 'Gets or sets the port to connect to on the remote server', {
+	usage: 'server [port]',
+}, function(args) {
+	if (args) {
+		conf.set('port', args);
+		conf.push();
+	}
+	console.log('server port: ' + conf.get('port'));
+});
+
+console.log('Welcome to the Word Guessing Game!\n');
+cli.run();
