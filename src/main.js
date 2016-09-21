aad5aee099253b63565c4460aed05eafa98be56a
John Call <johnkc1992@gmail.com>
2016-09-20 22:26:01 -0600

    Add settings for first/last name and a#

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ var conf = require('./conf.js')({
 	defaults: {
 		server: '127.0.0.1',
 		port: 12001,
+		fname: 'First',
+		lname: 'Last',
+		sid: 'Axxxxxxxx',
 	}
 });
 
@@ -26,6 +29,28 @@ cli.script('port', 'Gets or sets the port to connect to on the remote server', {
 		conf.push();
 	}
 	console.log('server port: ' + conf.get('port'));
+});
+
+cli.script('sid', 'Gets or sets the student A#', {
+	usage: 'sid [axxxxxxxx]',
+}, function(args) {
+	if (args) {
+		conf.set('sid', args);
+		conf.push();
+	}
+	console.log('A#: ' + conf.get('sid'));
+});
+
+cli.script('name', 'Gets or sets the student name', {
+	usage: 'name [first last]',
+}, function(args) {
+	if (args) {
+		var parts = args.split(' ');
+		conf.set('fname', parts[0]);
+		conf.set('lname', parts[1]);
+		conf.push();
+	}
+	console.log('Name: ' + conf.get('fname') + ' ' + conf.get('lname'));
 });
 
 cli.script('new', 'Exits the current game and starts a new one', {}, function(args) {
