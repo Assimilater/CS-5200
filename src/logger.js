f8352d2465c6a411a64f57a1475a6ec0b64e8193
John Call <johnkc1992@gmail.com>
2016-09-21 12:20:14 -0600

    Implement logger

---
+++ src/logger.js
@@ -0,0 +1,86 @@
+require('datejs');
+const fs = require('fs');
+const ps = require('process');
+const path = require('path');
+const winston = require('winston');
+const ns = require('./namespace.js');
+
+module.exports = function($ns, prefix) {
+	if (!($ns instanceof ns)) { 
+		console.log('Fatal error: invalid namspace provided to logger');
+		console.log($ns);
+		ps.exit();
+		return;
+	}
+	init(ns.addPlugin($ns, 'logger'), prefix);
+}
+
+function init(ready, prefix) {
+	if (typeof prefix !== 'string') {
+		prefix = '';
+	} else {
+		prefix = prefix + '-';
+	}
+	
+	// All logs collected in a 'log' folder with a date-stamped name and an iteration number
+	// Related to how many times the application was started today
+	var stamp = Date.today().toString('yyyy-MM-dd');
+
+	// Obtained from: http://stackoverflow.com/a/21196961/310560
+	function ensureExists(path, mask, cb) {
+		if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+			cb = mask;
+			mask = 0777;
+		}
+		fs.mkdir(path, mask, function(err) {
+			if (err) {
+				if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+				else cb(err); // something else went wrong
+			} else cb(null); // successfully created folder
+		});
+	}
+
+	// Check the existence of the log directory
+	ensureExists('log', function(err) {
+		if (err) {
+			console.log('Fatal error: unable to create log root! Error dump:')
+			console.log(err);
+			ps.exit();
+			return;
+		}
+		
+		// Callback for creating the log file upon determining a valid name
+		var init = function(file) {
+			ready(new (winston.Logger)({
+				transports: [
+					new winston.transports.File({ filename: file }),
+				],
+			}));
+		};
+		
+		// Recursively handle async checking for a valid file
+		(function next(i) {
+			var file = path.join('log', `${prefix}${stamp}.${i}.log`);
+			fs.stat(file, function(err, stats) {
+				if (err) {
+					if (err.code === 'ENOENT') {
+						// File doesn't exit; good to go
+						init(file);
+					} else {
+						// Something unexpected happened
+						console.log('Fatal error: unable to create log file! Error dump:')
+						console.log(err);
+						ps.exit();
+						return;
+					}
+				} else if (stats.isFile()) {
+					// 
+					next(++i);
+				} else {
+					// Directory of the same name exists; also good to go
+					init(file);
+				}
+			});
+		})(0);
+	});
+}
